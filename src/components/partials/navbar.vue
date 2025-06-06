c7f0fb0d3c67ef84c21c79b9a9e47ffc83a581bf
Habacuque <habacuke14@gmail.com>
2025-06-06 11:49:11 -0300

    styling filter and search component

--- src/components/partials/navbar.vue
+++ src/components/partials/navbar.vue
@@ -3,7 +3,7 @@
 
     <nav class="navbar navbar-expand-lg navbar-dark bg-primary" data-bs-theme="dark" v-if="isAuth">
         <div class="container-fluid">
-            <a class="navbar-brand" href="/">Growup</a>
+            <a class="navbar-brand" href="/">OpenKnowlodge</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
             </button>
