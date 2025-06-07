a2f259107cade6c04537a681ea4aa0eecca5e8a9
Habacuque <habacuke14@gmail.com>
2025-06-07 18:55:05 -0300

    update options in navbar

--- src/components/partials/navbar.vue
+++ src/components/partials/navbar.vue
@@ -10,20 +10,24 @@
             <div class="collapse navbar-collapse" id="navbarNavDropdown">
             <ul class="navbar-nav">
                 <li class="nav-item">
-                    <RouterLink to="/index/" class="nav-link active" >Home</RouterLink>
+                    <RouterLink to="/index/" class="nav-link active" >Início</RouterLink>
                 </li>
                 <li class="nav-item">
-                    <RouterLink to="/course/add-course" class="nav-link">Criar curso<i class="bi bi-add"></i></RouterLink>
+                    <RouterLink to="/index/" class="nav-link active" ><i class="bi bi-bell-fill"></i></RouterLink>
                 </li>
                 <li class="nav-item dropdown">
                     <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                        Profile
+                        <i class="bi bi-person-fill"></i>
                     </a>
                     <ul class="dropdown-menu">
                         <div v-if="isAuth">
                             <RouterLink to="/user/profile/" class="dropdown-item"><i class="bi bi-person"></i> Profile</RouterLink>
-                            <!-- <a href="/user/profile/" class="dropdown-item"><i class="bi bi-person"></i> Profile</a> -->
-                            <a @click="logout()" class="dropdown-item"><i class="bi bi-arrow-left"></i> Logout</a>
+                            <RouterLink to="/user/profile/" class="dropdown-item"><i class="bi bi-kanban"></i> Cursos em andamento</RouterLink>
+                            <RouterLink to="/user/profile/" class="dropdown-item"><i class="bi bi-gear"></i> Configurações</RouterLink>
+                            <RouterLink to="/user/profile/" class="dropdown-item"><i class="bi bi-arrow-down-circle"></i> Atualizações</RouterLink>
+                            <RouterLink to="/user/profile/" class="dropdown-item"><i class="bi bi-question-circle"></i> Central de ajuda</RouterLink>
+                            <RouterLink to="/course/add-course" class="dropdown-item" ><i class="bi bi-lock"></i> Admin</RouterLink>
+                            <a @click="logout()" class="dropdown-item"><i class="bi bi-arrow-left"></i> Sair</a>
                         </div>
                         <div v-else>
                             <RouterLink to="/user/login/" class="dropdown-item"><i class="bi bi-person-circle"></i> Login</RouterLink>
